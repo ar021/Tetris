f6def05fab4840323914817f5599e66cebe7d175
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-03-30 23:05:17 -0400

    Added moveDown functions

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
 const gameBoard = document.querySelector('.game-board');
-const cellsArray = document.querySelectorAll('.cell');
+const cellsArray = document.querySelectorAll('.game-board div');
 console.log(gameBoard);
 console.log(cellsArray);
 
-const boardWidth = 5;
-const initPosition = 0;
+const bWidth = 5;
+let currentPosition = 0;
 
 // Tetros
-const lTetro = [0,0+boardWidth, (0+boardWidth)*2, (0+boardWidth)*2+1];
+const lTetro = [0,0+bWidth, (0+bWidth)*2, (0+bWidth)*2+1];
+console.log(lTetro);
+
+// Function
+function render(){
+    lTetro.forEach(function(i){
+        cellsArray[currentPosition + i].classList.add('cell');
+    });
+}
+render();
+
+function unRender(){
+    lTetro.forEach(function(i){
+        cellsArray[currentPosition + i].classList.remove('cell');
+    });
+}
+// unRender();
+
+function moveDown(){
+    unRender();
+    console.log(`before ${currentPosition}`);
+    // if(currentPosition<35){
+    //     currentPosition = currentPosition + 5;
+        
+    // }else{
+    //     currentPosition = 0;
+    // }
+    // console.log(`After: ${currentPosition}`);
+    currentPosition = currentPosition + 5;
+    render();
+    stop();
+}
+
+
+var intervalId = window.setInterval(function(){
+    moveDown();
+  }, 1000);
+
+// function stop(){
+//     const w = 5; 
+//     function stopDecider(){
+//       let s = lTetro.some(function(i){
+//         return cellsArray[i+w].hasAttribute('class','stop');
+//         console.log(cellsArray[i+w]);
+//         });
+//         return s;
+//     };
+//     if(stopDecider){
+//         clearInterval(intervalId);  
+//     }
+//     else return;
+// }
