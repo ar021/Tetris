07895f0812d5fc34b89c6a9a65d76e70c036e51e
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-03-30 20:53:04 -0400

    Added gameboard divs

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('tetris');
+const gameBoard = document.querySelector('.game-board');
+const cellsArray = document.querySelectorAll('.cell');
+console.log(gameBoard);
+console.log(cellsArray);
+
+const boardWidth = 5;
+const initPosition = 0;
+
+// Tetros
+const lTetro = [0,0+boardWidth, (0+boardWidth)*2, (0+boardWidth)*2+1];
