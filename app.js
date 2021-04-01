b0a7a9163e29d93bbb13d2200bdbd0accc22cdf6
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-03-31 22:34:08 -0400

    Added rest of the Tetros and added leftmove function

--- app.js
+++ app.js
@@ -4,25 +4,68 @@ console.log(gameBoard);
 console.log(cellsArray);
 
 const bWidth = 10;
-let currentPosition = 4;
+
 
 // Tetros
-const lTetro = [1,1+bWidth, 1+(bWidth*2), 2+(bWidth*2)];
-console.log(lTetro);
+const lTetro = [
+    [1, bWidth + 1, bWidth * 2 + 1, 2],
+    [bWidth, bWidth + 1, bWidth + 2, bWidth * 2 + 2],
+    [1, bWidth + 1, bWidth * 2 + 1, bWidth * 2],
+    [bWidth, bWidth * 2, bWidth * 2 + 1, bWidth * 2 + 2]
+];
+const iTetro = [
+    [1, bWidth + 1, bWidth * 2 + 1, bWidth * 3 + 1],
+    [bWidth, bWidth + 1, bWidth + 2, bWidth + 3],
+    [1, bWidth + 1, bWidth * 2 + 1, bWidth * 3 + 1],
+    [bWidth, bWidth + 1, bWidth + 2, bWidth + 3]
+];
+const tTetro = [
+    [1, bWidth, bWidth + 1, bWidth + 2],
+    [1, bWidth + 1, bWidth + 2, bWidth * 2 + 1],
+    [bWidth, bWidth + 1, bWidth + 2, bWidth * 2 + 1],
+    [1, bWidth, bWidth + 1, bWidth * 2 + 1]
+];
+const zTetro = [
+    [0, bWidth, bWidth + 1, bWidth * 2 + 1],
+    [bWidth + 1, bWidth + 2, bWidth * 2, bWidth * 2 + 1],
+    [0, bWidth, bWidth + 1, bWidth * 2 + 1],
+    [bWidth + 1, bWidth + 2, bWidth * 2, bWidth * 2 + 1]
+];
+const oTetro = [
+    [0, 1, bWidth, bWidth + 1],
+    [0, 1, bWidth, bWidth + 1],
+    [0, 1, bWidth, bWidth + 1],
+    [0, 1, bWidth, bWidth + 1]
+];
+const tetros = [lTetro, iTetro, tTetro, zTetro, oTetro];
+
+// Current Position & Tetro
+let currentPosition = 4;
+let currentRoatation = 0;
+let random = Math.floor(Math.random() * tetros.length); // random selection
+let currentTetro = tetros[random][currentRoatation];
+console.log(currentTetro);
+
+
+
+
+
+
+
 
 // Function
 function render(){
     console.log('render run');
-    lTetro.forEach(function(i){
+    currentTetro.forEach(function(i){
         cellsArray[currentPosition + i].classList.add('cell');
     });
 
 }
-render();
+// render();
 
 function unRender(){
     console.log('unRender run');
-    lTetro.forEach(function(i){
+    currentTetro.forEach(function(i){
         cellsArray[currentPosition + i].classList.remove('cell');
     });
 }
@@ -53,7 +96,7 @@ var intervalId = window.setInterval(function(){
 function stop(){
     console.log('stop run');
     function stopDecider(){
-      return lTetro.some(function(i){
+      return currentTetro.some(function(i){
         // return cellsArray[currentPosition + i + w].hasAttribute('class','stop');
         // console.log(cellsArray[currentPosition + i + w]);
         console.log('stopDecider run');
@@ -61,11 +104,38 @@ function stop(){
         });
     };
     if(stopDecider()){
-        lTetro.forEach(function(i){
+        currentTetro.forEach(function(i){
             cellsArray[currentPosition + i].classList.add('stop');
         });
         currentPosition = 4;
+        random = Math.floor(Math.random() * tetros.length);
+        currentTetro = tetros[random][currentRoatation];
         render();
     }
+    return;
+}
+
+function filled(){
+    currentTetro.some(function(i){
+        console.log('filled run');
+        return cellsArray[currentPosition + i ].classList.contains('stop');
+    });
+}
+
+function left(){
+    unRender();
+    function leftDecider(){
+        return currentTetro.some(function(i){
+          console.log('leftDecider run');
+          return (currentPosition + i) % bWidth === 0;
+        });
+    }
+    if(!leftDecider()){
+        currentPosition = currentPosition - 1;
+        if(filled()){
+            currentPosition = currentPosition + 1;
+        }
+        render();
+    } 
     return;
 }
